f44cfbccdf872df8935742fa28916e58a3fbac03
GI HOSP <37694911+gihosp@users.noreply.github.com>
2019-12-21 14:09:03 +0530

    Update cordova_plugin_giaes.js

--- www/cordova_plugin_giaes.js
+++ www/cordova_plugin_giaes.js
@@ -1,17 +1,18 @@
-function cordova_plugin_cordova_plugin_giaes(){
+
+function cordova_plugin_giaes(){
 }
 
-cordova_plugin_cordova_plugin_giaes.prototype.encrypt = function (options, successCallback,errorCallback) {
+cordova_plugin_giaes.prototype.encrypt = function (options, successCallback,errorCallback) {
   cordova.exec(successCallback, errorCallback, "cordova_plugin_giaes", "encrypt",[options]);
 };
-cordova_plugin_cordova_plugin_giaes.prototype.encryptnoencode = function (options, successCallback,errorCallback) {
+cordova_plugin_giaes.prototype.encryptnoencode = function (options, successCallback,errorCallback) {
   cordova.exec(successCallback, errorCallback, "cordova_plugin_giaes", "encryptnoencode",[options]);
 };
-cordova_plugin_cordova_plugin_giaes.prototype.decrypt = function (options, successCallback,errorCallback) {
+cordova_plugin_giaes.prototype.decrypt = function (options, successCallback,errorCallback) {
   cordova.exec(successCallback, errorCallback, "cordova_plugin_giaes", "decrypt",[options]);
 };
 
-cordova_plugin_cordova_plugin_giaes.install = function () {
+cordova_plugin_giaes.install = function () {
   if (!window.plugins) {
     window.plugins = {};
   }
